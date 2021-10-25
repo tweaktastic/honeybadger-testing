3798b07275fd227903ada15dccf19d0c798a0185
Abhinav Sharma <abhinavsharma333@gmail.com>
2021-10-25 16:15:34 -0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,29 @@
+import { promisify } from 'util';
 import handler from './index';
 
+const wrappedHandler = promisify(handler);
 
 describe('Handler test', () => {
-  test('throws an error', (done) => {
-    handler({}, {}, (err?: Error, resp?: any) => {
-      if(err) {
-        console.log('gets here');
-        done();
-      }
-      done(new Error('Test should fail'));
-    });
+  test('throws an error', async () => {
+    try {
+      await wrappedHandler({
+        fail: 1
+      }, {});
+      throw new Error('Should fail')
+    } catch(err) {
+      console.log(err);
+    }
+  });
+
+  test('throws no error', async () => {
+    try {
+      const a: any = await wrappedHandler({
+        fail: 0
+      }, {});
+      expect(a.statusCode).toBe(1);
+      expect(a.body).toBe('something');
+    } catch(err) {
+      throw err;
+    }
   });
 });
